7185f1eed9a1eafb1550b43f2c96b89b51ecaf58
noahneras <110417604+noahneras@users.noreply.github.com>
2024-01-27 12:49:19 -0500

    Wwise Music Events
    Adding music events from wwise and generated soundbank

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,115 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID CHARACTERSPEECH = 3610183652U;
+        static const AkUniqueID LAUGHTRACK = 907177173U;
+        static const AkUniqueID PLAY_ALMILLERSTINGER = 2331873856U;
+        static const AkUniqueID PLAY_BILLYSTINGER = 549438392U;
+        static const AkUniqueID PLAY_GAMECOMPLETE = 4194000609U;
+        static const AkUniqueID PLAY_MARGARETSTINGER = 4226694603U;
+        static const AkUniqueID PLAY_THEME = 2479442773U;
+        static const AkUniqueID STOP_THEME = 560299279U;
+        static const AkUniqueID UI_CURSOR = 2882502644U;
+        static const AkUniqueID UI_SELECT = 2774129122U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace AMBROOM
+        {
+            static const AkUniqueID GROUP = 885864942U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID BILLYSROOM = 2107887799U;
+                static const AkUniqueID DEN = 378875106U;
+                static const AkUniqueID DININGROOM = 2262472791U;
+                static const AkUniqueID GARAGE = 311707310U;
+                static const AkUniqueID KITCHEN = 1586158131U;
+                static const AkUniqueID MAINBEDROOM = 2819021838U;
+                static const AkUniqueID PORCH = 2157048949U;
+                static const AkUniqueID UTILITYROOM = 3450058598U;
+            } // namespace SWITCH
+        } // namespace AMBROOM
+
+        namespace CHARACTERVOICE
+        {
+            static const AkUniqueID GROUP = 3747948696U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID ALMILLER = 2732701073U;
+                static const AkUniqueID BILLY = 818946243U;
+                static const AkUniqueID DALE = 1825249057U;
+                static const AkUniqueID HATGUY = 1619719889U;
+                static const AkUniqueID MARGARET = 3460511428U;
+                static const AkUniqueID WATCHER = 746475195U;
+            } // namespace SWITCH
+        } // namespace CHARACTERVOICE
+
+        namespace CHARACTERVOICETONE
+        {
+            static const AkUniqueID GROUP = 2032718168U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID APPROVE = 1323806540U;
+                static const AkUniqueID DISAPPROVE = 1297835106U;
+                static const AkUniqueID NEUTRAL = 670611050U;
+            } // namespace SWITCH
+        } // namespace CHARACTERVOICETONE
+
+        namespace LAUGHTRACKSTATE
+        {
+            static const AkUniqueID GROUP = 2420739616U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID FULL = 2510516222U;
+                static const AkUniqueID GONE = 685410452U;
+                static const AkUniqueID HALF = 3633416822U;
+                static const AkUniqueID NEARFULL = 751093624U;
+                static const AkUniqueID NEARGONE = 2759656302U;
+            } // namespace SWITCH
+        } // namespace LAUGHTRACKSTATE
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID GAMEPROGRESS = 1577714322U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID GENERAL = 133642231U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
