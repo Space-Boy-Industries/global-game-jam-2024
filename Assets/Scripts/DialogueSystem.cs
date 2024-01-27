c722307b010161e9eacd8a5bc9a21f3d27d2bb2a
Eric Sexton <foopis23@gmail.com>
2024-01-27 14:43:10 -0500

    added player to main scene

--- Assets/Scripts/DialogueSystem.cs
+++ Assets/Scripts/DialogueSystem.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using Ink.Runtime;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
