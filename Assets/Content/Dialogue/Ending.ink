a110c6bb1c12cf41d8236e5fb9105e60d01e239f
Eric Sexton <foopis23@gmail.com>
2024-01-28 18:43:41 -0500

    pee

---
+++ Assets/Content/Dialogue/Ending.ink
@@ -0,0 +1,20 @@
+﻿EXTERNAL setFlag(variableName, value)
+EXTERNAL loadScene(sceneName)
+EXTERNAL playSound(soundName)
+EXTERNAL playAnimation(animationName, persist)
+
+VAR in_sitcom = false
+VAR cleaned_table = false
+VAR asked_for_coffee = false
+VAR has_coffee = false
+VAR coffee_correct = false
+VAR coffee_done = false
+VAR did_homework = false
+VAR built_gundam = false
+VAR gave_life_advice = false
+VAR talked_about_life = false
+
+VAR talked_to_al_phase_1 = false
+VAR talked_to_al_phase_2 = false
+
+DIRECTOR: CUT, We are going to have to take that again.
---
+++ Assets/EndingHandler.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using Ink.Runtime;
+using UnityEngine;
+
+public class EndingHandler : MonoBehaviour
+{
+    public AK.Wwise.Event endingTheme;
+    public TextAsset endingText;
+    private Story endingStory;
+    
+    private void Start()
+    {
+        endingStory = new Story(endingText.text);
+        GlobalStateSystem.Instance.OnGlobalStateUpdate.AddListener((context) =>
+        {
+            if (context.VariableName == "talked_about_life" && context.Value.ToString().ToLower() == "true")
+            {
+                StartCoroutine(EndingCoroutine());
+            }
+        });
+    }
+    
+    private IEnumerator EndingCoroutine()
+    {
+        yield return new WaitForSeconds(6.0f);
+        endingTheme.Post(gameObject);
+        yield return new WaitForSeconds(1.0f);
+        if (DialogueSystem.Instance.isOpen)
+        {
+            DialogueSystem.Instance.CloseDialogue();
+        }
+        DialogueSystem.Instance.SetStory(endingStory);
+        DialogueSystem.Instance.OpenDialogue();
+    }
+}
