152e3236595701efeb387fd3cb6c52767dfcc736
Eric Sexton <foopis23@gmail.com>
2024-01-28 17:21:08 -0500

    clean table logic

---
+++ Assets/CleanTableLogic.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CleanTableLogic : MonoBehaviour
+{
+    public AK.Wwise.Event CleanTableSound;
+
+    public void CleanTable()
+    {
+        StartCoroutine(CleanTableCoroutine());
+    }
+    
+    private IEnumerator CleanTableCoroutine()
+    {
+        CleanTableSound.Post(gameObject);
+        yield return new WaitForSeconds(1f);
+        GlobalStateSystem.Instance.SetFlag("cleaned_table", true);
+        Destroy(gameObject);
+    }
+}
